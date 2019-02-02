263902001d43ba028dde7f4884797bcce8ed0f91
Michael Clark <michaeljclark@mac.com>
2019-02-03 11:12:35 +1300

    RISC-V: convert custom spinlock/rwlock to generic qspinlock/qrwlock
    Update the RISC-V port to use the generic qspinlock and qrwlock.
    
    This patch requires support for xchg and cmpxchg for small words
    (char and short) which are added by a previous patch.
    
    Cc: RISC-V Patches <patches@groups.riscv.org>
    Cc: Linux RISC-V <linux-riscv@lists.infradead.org>
    Signed-off-by: Michael Clark <michaeljclark@mac.com>

---
+++ arch/riscv/include/asm/mcs_spinlock.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_RISCV_MCS_SPINLOCK_H
+#define _ASM_RISCV_MCS_SPINLOCK_H
+
+#include <asm-generic/mcs_spinlock.h>
+
+#endif /* _ASM_MCS_SPINLOCK_H */
